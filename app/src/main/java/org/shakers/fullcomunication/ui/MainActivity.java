dc4af6fa55b844c2c6c58b575f34905d1a706ac0
ひもの <128397176+Himoooooono@users.noreply.github.com>
2024-06-07 14:26:12 +0900

    TitleFragmentからGenreFragmentに遷移できるようにボタンを配置

--- app/src/main/java/org/shakers/fullcomunication/ui/MainActivity.java
+++ app/src/main/java/org/shakers/fullcomunication/ui/MainActivity.java
@@ -2,11 +2,7 @@ package org.shakers.fullcomunication.ui;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import org.shakers.fullcomunication.R;
 
@@ -15,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
         // Title Fragmentを読み込むようにする
--- app/src/main/java/org/shakers/fullcomunication/ui/TitleFragment.java
+++ app/src/main/java/org/shakers/fullcomunication/ui/TitleFragment.java
@@ -1,12 +1,13 @@
 package org.shakers.fullcomunication.ui;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
 
 import org.shakers.fullcomunication.R;
 
@@ -60,7 +61,19 @@ public class TitleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title, container, false);
+        View view = inflater.inflate(R.layout.fragment_title, container, false);
+        //Fragmentの場合
+        Button startButton = view.findViewById(R.id.start_button);
+        startButton.setOnClickListener(v -> {
+            Log.d("TitleFragment", "onClick: startButton");
+
+            // ジャンル選択画面に遷移
+            requireActivity().getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.nav_host_fragment, new GenreChoiceFragment())
+                    .commit();
+
+        });
+
+        return view;
     }
 }
