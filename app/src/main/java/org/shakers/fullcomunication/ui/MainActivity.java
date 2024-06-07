6b17282a6698e95c3eb110c4e382691950d24418
ひもの <128397176+Himoooooono@users.noreply.github.com>
2024-06-07 10:13:18 +0900

    Make ui package make title fragment

--- app/src/main/java/org/shakers/fullcomunication/ui/MainActivity.java
+++ app/src/main/java/org/shakers/fullcomunication/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package org.shakers.fullcomunication;
+package org.shakers.fullcomunication.ui;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import org.shakers.fullcomunication.R;
 
 public class MainActivity extends AppCompatActivity {
 
