03807a2ea47c115735c5d56117a1cc6a499885a1
ひもの <128397176+Himoooooono@users.noreply.github.com>
2024-06-11 14:31:56 +0900

    WIP:MainActivityでShakeDetectorクラスの処理を確認した

--- app/src/main/java/org/shakers/fullcomunication/ui/MainActivity.java
+++ app/src/main/java/org/shakers/fullcomunication/ui/MainActivity.java
@@ -1,21 +1,37 @@
 package org.shakers.fullcomunication.ui;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import org.shakers.fullcomunication.R;
+import org.shakers.fullcomunication.sensor.ShakeDetector;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SensorManager mSensorManager;
+    private ShakeDetector mShakeDetector;
+    private Sensor linearAccelerometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        linearAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+
+        mShakeDetector = new ShakeDetector(new ShakeDetector.OnShakeListener() {
+            @Override
+            public void onShake() {
+                Toast.makeText(MainActivity.this, "Shake!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mSensorManager.registerListener(mShakeDetector, linearAccelerometer, SensorManager.SENSOR_DELAY_UI);
+
     }
 }
