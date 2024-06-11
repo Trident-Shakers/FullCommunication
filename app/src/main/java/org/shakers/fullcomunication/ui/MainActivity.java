d470214135c27773efdf2118d72e147312ea5318
ひもの <128397176+Himoooooono@users.noreply.github.com>
2024-06-11 14:43:09 +0900

    WIP:MainActivityにloadFragment(Fragment fragment)を追加

--- app/src/main/java/org/shakers/fullcomunication/ui/MainActivity.java
+++ app/src/main/java/org/shakers/fullcomunication/ui/MainActivity.java
@@ -3,6 +3,8 @@ package org.shakers.fullcomunication.ui;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import org.shakers.fullcomunication.R;
 
@@ -17,5 +19,20 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.nav_host_fragment, new TitleFragment())
                 .commit();
+    }
+
+    /**
+     * Fragmentを読み込めます。
+     * <p>
+     * Fragmentで使う場合の例: getActivity().loadFragment(new GenreChoiceFragment());
+     * </p>
+     *
+     * @param fragment - 読み込むFragmentをnew FragmentName()で指定してください。
+     */
+    public void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.nav_host_fragment, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
