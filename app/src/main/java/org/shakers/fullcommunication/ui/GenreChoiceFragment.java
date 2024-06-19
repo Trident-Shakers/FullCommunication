bc59b908e0fcf649443c4d9e3fcc917f32e7a521
3mi-pc <123@123>
2024-06-20 02:54:47 +0900

    レイアウトだけして何もしてません

--- app/src/main/java/org/shakers/fullcommunication/ui/GenreChoiceFragment.java
+++ app/src/main/java/org/shakers/fullcommunication/ui/GenreChoiceFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
