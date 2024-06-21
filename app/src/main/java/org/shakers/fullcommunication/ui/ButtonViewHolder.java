a562cec24365d524ee1c345b48109224631fc8de
みみみ <232250@trident.ac.jp>
2024-06-21 12:03:40 +0900

    onBindViewHolderが動かない涙

--- app/src/main/java/org/shakers/fullcommunication/ui/ButtonViewHolder.java
+++ app/src/main/java/org/shakers/fullcommunication/ui/ButtonViewHolder.java
@@ -3,13 +3,14 @@ package org.shakers.fullcommunication.ui;
 import android.view.View;
 
 import android.widget.Button;
+import android.widget.ToggleButton;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.shakers.fullcommunication.R;
 
 public class ButtonViewHolder extends RecyclerView.ViewHolder {
-    public Button button;
+    public ToggleButton button;
 
     public ButtonViewHolder(View view) {
         super(view);
