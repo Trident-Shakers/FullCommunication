95b86535159ff903cf404e1ff8ca071c1103fb37
ひもの <128397176+Himoooooono@users.noreply.github.com>
2024-06-26 15:44:59 +0900

    feat:モックデータの生成と格納ができるようにした

--- app/src/main/java/org/shakers/fullcommunication/ui/TopicFragment.java
+++ app/src/main/java/org/shakers/fullcommunication/ui/TopicFragment.java
@@ -1,5 +1,7 @@
 package org.shakers.fullcommunication.ui;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -14,6 +16,7 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 
 import org.shakers.fullcommunication.R;
+import org.shakers.fullcommunication.data.TopicCountMockData;
 
 public class TopicFragment extends Fragment {
 
@@ -54,6 +57,13 @@ public class TopicFragment extends Fragment {
         mButtonFinish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                TopicCountMockData topicCountMockData = new TopicCountMockData();
+                requireActivity();
+                SharedPreferences.Editor editor = requireActivity().getSharedPreferences("topic_time_count_list", Context.MODE_PRIVATE).edit();
+                Log.d("TopicFragment", topicCountMockData.getTopicTimeListString());
+                editor.putString("topic_time_count_list", topicCountMockData.getTopicTimeListString());
+                editor.apply();
+
                 ((MainActivity) requireActivity()).loadFragment(new ResultFragment());
             }
         });
