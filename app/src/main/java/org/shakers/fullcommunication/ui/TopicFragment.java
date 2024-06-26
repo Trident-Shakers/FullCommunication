ec27d635756174271b42b21297036bfc5057cdef
ひもの <128397176+Himoooooono@users.noreply.github.com>
2024-06-26 15:08:41 +0900

    feat:mButtonFinishに画面遷移の機能を実装

--- app/src/main/java/org/shakers/fullcommunication/ui/TopicFragment.java
+++ app/src/main/java/org/shakers/fullcommunication/ui/TopicFragment.java
@@ -15,51 +15,16 @@ import android.widget.FrameLayout;
 
 import org.shakers.fullcommunication.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TopicFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TopicFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public TopicFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TopicFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TopicFragment newInstance(String param1, String param2) {
-        TopicFragment fragment = new TopicFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -83,6 +48,13 @@ public class TopicFragment extends Fragment {
             public void onClick(View v) {
                 Animation scaleUp = AnimationUtils.loadAnimation(getContext(), R.anim.scaleanimation);
                 frameLayout.startAnimation(scaleUp);
+            }
+        });
+
+        mButtonFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainActivity) requireActivity()).loadFragment(new ResultFragment());
             }
         });
 
