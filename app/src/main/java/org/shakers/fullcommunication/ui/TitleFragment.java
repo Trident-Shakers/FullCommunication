c6a22e376db41c7566a0a46af03167bd6ea0de86
ひもの <128397176+Himoooooono@users.noreply.github.com>
2024-07-02 15:35:13 +0900

    TitleFragmentの警告を除去

--- app/src/main/java/org/shakers/fullcommunication/ui/TitleFragment.java
+++ app/src/main/java/org/shakers/fullcommunication/ui/TitleFragment.java
@@ -11,51 +11,14 @@ import androidx.fragment.app.Fragment;
 
 import org.shakers.fullcommunication.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TitleFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TitleFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public TitleFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TitleFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TitleFragment newInstance(String param1, String param2) {
-        TitleFragment fragment = new TitleFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -72,10 +35,8 @@ public class TitleFragment extends Fragment {
                     .replace(R.id.nav_host_fragment, new GenreChoiceFragment())
                     .commit();
         });
-        Button hwubutton = view.findViewById(R.id.button_how_to_play);
-        hwubutton.setOnClickListener(v -> {
-            ((MainActivity) getActivity()).loadFragment(new HowToPlayFragment());
-        });
+        Button htpbutton = view.findViewById(R.id.button_how_to_play);
+        htpbutton.setOnClickListener(v -> ((MainActivity) requireActivity()).loadFragment(new HowToPlayFragment()));
         return view;
     }
 }
