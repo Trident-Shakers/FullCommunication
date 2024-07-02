1717a37332a5e5949e14cb87762f3fbffc345f50
ひもの <128397176+Himoooooono@users.noreply.github.com>
2024-07-02 15:40:30 +0900

    GenreChoiceFragmentの警告を削除し、いらない記述を削除

--- app/src/main/java/org/shakers/fullcommunication/ui/GenreChoiceFragment.java
+++ app/src/main/java/org/shakers/fullcommunication/ui/GenreChoiceFragment.java
@@ -16,51 +16,16 @@ import com.google.android.material.appbar.MaterialToolbar;
 
 import org.shakers.fullcommunication.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link GenreChoiceFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Objects;
+
 public class GenreChoiceFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public GenreChoiceFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GenreChoiceFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GenreChoiceFragment newInstance(String param1, String param2) {
-        GenreChoiceFragment fragment = new GenreChoiceFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -72,7 +37,7 @@ public class GenreChoiceFragment extends Fragment {
         MaterialToolbar toolbar = view.findViewById(R.id.topAppBarGC);
 
         ((AppCompatActivity) requireActivity()).setSupportActionBar(toolbar);
-        ((AppCompatActivity) requireActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Objects.requireNonNull(((AppCompatActivity) requireActivity()).getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
         toolbar.setNavigationOnClickListener(v -> {
             // Handle the back button event
