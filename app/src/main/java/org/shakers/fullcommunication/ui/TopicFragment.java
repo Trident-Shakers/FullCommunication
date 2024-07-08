10824b301ec89407f848eb8edd3567fd7f7c3b4d
ひもの <128397176+Himoooooono@users.noreply.github.com>
2024-07-08 19:09:27 +0900

    話題提供に関わるロジックを切り出す

--- app/src/main/java/org/shakers/fullcommunication/ui/TopicFragment.java
+++ app/src/main/java/org/shakers/fullcommunication/ui/TopicFragment.java
@@ -14,10 +14,9 @@ import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 
 import org.shakers.fullcommunication.R;
+import org.shakers.fullcommunication.data.TopicCountData;
 import org.shakers.fullcommunication.ui.animation.AnimationHelper;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class TopicFragment extends Fragment {
@@ -26,6 +25,9 @@ public class TopicFragment extends Fragment {
     private int debugButtonClickCount = 0;
     private long debugButtonPressTime = 0;
     private long finishButtonPressTime = 0;
+
+    TopicCountData topicCountData;
+    List<String> topicList;
 
     public TopicFragment() {
         // Required empty public constructor
@@ -57,21 +59,13 @@ public class TopicFragment extends Fragment {
         });
 
         TextView topicText = view.findViewById(R.id.topic);
-        String[] topicValues = getTopicValues();
+        topicCountData = new TopicCountData(requireActivity());
+        topicList = topicCountData.getTopicList();
 
         debugStartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 最初にdebugButtonが押された時刻を記録
-                if (debugButtonClickCount == 0) {
-                    debugButtonPressTime = System.currentTimeMillis();
-                }
-                // トピックを表示するロジックを更新
-                if (debugButtonClickCount < topicValues.length) {
-                    String topicValue = topicValues[debugButtonClickCount];
-                    topicText.setText(topicValue);
-                    debugButtonClickCount++;
-                }
+                changeTopic(topicText);
             }
         });
 
@@ -92,52 +86,50 @@ public class TopicFragment extends Fragment {
         finishButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (debugButtonClickCount > 0) { // debugButtonが少なくとも一度は押されていることを確認
-                    finishButtonPressTime = System.currentTimeMillis();
-                    long elapsedTime = finishButtonPressTime - debugButtonPressTime;
-                    Log.d("TopicFragment", "Elapsed Time: " + elapsedTime + "ms");
-                    // トピック表示を空白にする
-                    topicText.setText("");
-                    // 時間を保存するロジックを呼び出し
-                    if (debugButtonClickCount > 1) { // debugButtonが一度以上押された後にfinishButtonが押された場合
-                        TopicTimeData(topicValues[debugButtonClickCount - 2], elapsedTime);
-                    }
-                }
+                saveTopicCount(topicText);
             }
         });
 
         return view;
     }
-
-    private String[] getTopicValues() {
-        SharedPreferences sharedPreferences = requireActivity().getSharedPreferences("DATA", Context.MODE_PRIVATE);
-        List<String> topicTimeList = new ArrayList<>();
-        for (int i = 1; i <= 21; i++) {
-            String key = String.valueOf(i);
-            String value = sharedPreferences.getString(key, null);
-            if (value != null) {
-                topicTimeList.add(value);
-            }
-        }
-        Collections.shuffle(topicTimeList);
-        return topicTimeList.toArray(new String[0]);
-    }
-
     private void TopicTimeData(String topic, long time) {
         SharedPreferences sharedPreferences = requireActivity().getSharedPreferences("topic_time_count_list", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         String value = sharedPreferences.getString("topic_time_count_list", null);
-
         //topic-timeの形式でStringに格納
         if (value != null) {
             value = value + "," + topic + "-" + time;
         } else {
             value = topic + "-" + time;
         }
-
         //作ったStringをSharedpreferencesに格納
         editor.putString("topic_time_count_list", value);
-
         editor.apply();
+    }
+
+    private void changeTopic(TextView tv){
+        // 最初にdebugButtonが押された時刻を記録
+        if (debugButtonClickCount == 0) {
+            debugButtonPressTime = System.currentTimeMillis();
+        }
+        // トピックを表示するロジックを更新
+        if(debugButtonClickCount < topicList.size()){
+            tv.setText(topicList.get(debugButtonClickCount));
+            debugButtonClickCount++;
+        }
+    }
+
+    private void saveTopicCount(TextView tv){
+        if (debugButtonClickCount > 0) { // debugButtonが少なくとも一度は押されていることを確認
+            finishButtonPressTime = System.currentTimeMillis();
+            long elapsedTime = finishButtonPressTime - debugButtonPressTime;
+            Log.d("TopicFragment", "Elapsed Time: " + elapsedTime + "ms");
+            // トピック表示を空白にする
+            tv.setText("");
+            // 時間を保存するロジックを呼び出し
+            if (debugButtonClickCount > 1) { // debugButtonが一度以上押された後にfinishButtonが押された場合
+                TopicTimeData(topicList.get(debugButtonClickCount - 2), elapsedTime);
+            }
+        }
     }
 }
